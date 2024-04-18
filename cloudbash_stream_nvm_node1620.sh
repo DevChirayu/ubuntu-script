eccc5316636d5f0081987bd164ae7ac46b593c6b
DevChirayu <chirayupatil23@gmail.com>
2024-04-18 22:17:40 +0530

    node 16 20

--- cloudbash_stream_nvm_node1620.sh
+++ cloudbash_stream_nvm_node1620.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Install nvm
 echo "Installing nvm..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-# Activate nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Install Node.js version 20
+echo "Installing Node.js version 16..."
+nvm install 16
+
 echo "Installing Node.js version 20..."
 nvm install 20
 
@@ -16,5 +16,5 @@ nvm install 20
 echo "Node.js version installed:"
 node -v
 
-echo "You have successfully installed Node.js version 20 on your system."
+echo "You have successfully installed Node.js version 20 and 16 on your system."
 
